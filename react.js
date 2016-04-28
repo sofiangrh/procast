38eaf434ca8eaee8ce0124c47fdd82e0fde5a7be
sofiangrh <137006200@student.unsil.ac.id>
2016-04-28 18:51:42 +0700

    Create react.js

---
+++ react.js
@@ -0,0 +1,15 @@
+},
+  "keywords": [
+    "react",
+    "react-native",
+    "react-component",
+    "map",
+    "mapview",
+    "google-maps",
+    "mapkit"
+  ],
+  "dependencies": {},
+  "devDependencies": {
+    "react-native": "latest"
+  }
+}
